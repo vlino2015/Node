b8dd554e1bbde093d26c5bd3f36386387cccef59
Veronica <Veronica@digitalcrafts.com>
2018-04-10 20:12:11 -0500

    sync

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,25 @@
 var express = require('express');
 var router = express.Router();
-//var rocker = require('../util/hello.js');
+var myName = require('../util/hello.js');
 
 
 router.get('/', function(req, res){
-    var data = req.app.get('appData');
+    // var data = req.app.get('appData');
 
-    var pagePhotos = [];
+    // var pagePhotos = [];
 
-    data.speakers.forEach(function(item) {
-        pagePhotos = pagePhotos.concat(item.artwork);
+    // data.speakers.forEach(function(item) {
+    //     pagePhotos = pagePhotos.concat(item.artwork);
         
-    });
+    // });
     
-    res.render('index', {
-        pageTitle: 'Here is my variable',
-        pageID: 'Veronica',
-        artwork: pagePhotos
-    });
+    // res.render('index', {
+    //     pageTitle: 'Here is my variable',
+    //     pageID: 'Veronica',
+    //     artwork: pagePhotos
+    // });
 
-    //console.log(rocker.name('Veronica Lino')); 
+    console.log(myName.name('Veronica Lino')); 
 });
 
 module.exports = router;
